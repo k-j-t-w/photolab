bf12fbec02d016de9fa2cd1298d6b8e55958efc9
Kai Wingfield <kaiwingfield17@gmail.com>
2024-03-28 14:14:57 -0700

    Correct styling errors. Add Favourite button

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 const PhotoListItem = (props) => {
   return (
     <>
-      {console.log(props)}
         <div className='photo-list__item'>
+          <PhotoFavButton />
           <img src={props.photo.imageSource} alt="Photo" className='photo-list__image'/>
-          <img src={props.photo.profile} alt="Profile Photo" className='photo-list__user-profile' />
-          <div className='photo=list__user-info'>{props.photo.username}</div>
-          <div className='photo-list__user-location'>
-            {props.photo.location.city},
-            {props.photo.location.country}
+          <div className='photo-list__user-details'>
+            <img src={props.photo.profile} alt="Profile Photo" className='photo-list__user-profile' />
+            <div className='photo-list__user-info'>
+              {props.photo.username}
+              <div className='photo-list__user-location'>
+                {props.photo.location.city},
+                {props.photo.location.country}
+              </div>
+            </div>
           </div>
         </div>
     </>

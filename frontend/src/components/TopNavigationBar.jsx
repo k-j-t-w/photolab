6b7ab14137c299761cfc3fbb345884274fe9d5c8
Kai Wingfield <kaiwingfield17@gmail.com>
2024-03-29 21:13:32 -0700

    Change stylings

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -8,10 +8,8 @@ const TopNavigation = ({topics, favArr}) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <div className='top-nav-bar__right'>
         <TopicList topics={topics}/>
         <FavBadge selected={!!true} favArr={favArr}/>
-      </div>
     </div>
   )
 }

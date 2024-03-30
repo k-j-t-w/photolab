ce55ecbbb8a02bd8120c8636a9ed519d1e4a778c
Kai Wingfield <kaiwingfield17@gmail.com>
2024-03-29 21:02:20 -0700

    Add useReducer instead of useState along with required logic

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,56 +1,72 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
+// Defines initial State
+const initialState = {
+  favArr: [],
+  displayModal: false,
+  currentPhoto: null,
+};
+
+// Define actions
+export const ACTIONS = {
+  TOGGLE_FAV: 'TOGGLE_FAV',
+  OPEN_MODAL: 'OPEN_MODAL',
+  CLOSE_MODAL: 'CLOSE_MODAL',
+  SELECT_PHOTO_ID: 'SELECT_PHOTO_ID',
+};
+
+// Define reducer function that handles each action
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.TOGGLE_FAV:
+      return {
+        ...state,
+        favArr: state.favArr.includes(action.payload.id)
+          ? state.favArr.filter(favId => favId !== action.payload.id)
+          : [...state.favArr, action.payload.id],
+      };
+    case ACTIONS.OPEN_MODAL:
+      return {
+        ...state,
+        displayModal: true,
+      };
+    case ACTIONS.CLOSE_MODAL:
+      return {
+        ...state,
+        displayModal: false,
+      };
+    case ACTIONS.SELECT_PHOTO_ID:
+      return {
+        ...state,
+        currentPhoto: action.payload.id,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  };
+};
+
+// Declaring functions that call useReducer alongside our reducer function
 const useApplicationData = () => {
 
-  const [state, setState] = useState({
-    favArr: [],
-    displayModal: false,
-    currentPhoto: null,
-  })
-
-  const addToFavArr = (id) => {
-    setState(prevState => ({
-      ...prevState,
-      favArr: [...prevState.favArr, id]
-    }));
-  };
-
-  const removeFromFavArr = (id) => {
-    setState(prevState => ({
-      ...prevState,
-      favArr: prevState.favArr.filter(favId => favId !== id)
-    }));
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFav = (id) => {
-    if (state.favArr.includes(id)) {
-      removeFromFavArr(id);
-    } else {
-      addToFavArr(id);
-    }
+    dispatch({type: ACTIONS.TOGGLE_FAV, payload: { id }});
   };
 
   const openModal = () => {
-    setState(prevState => ({
-      ...prevState,
-      displayModal: true
-    }));
+    dispatch({ type: ACTIONS.OPEN_MODAL });
   };
 
   const closeModal = () => {
-    setState(prevState => ({
-      ...prevState,
-      displayModal: false
-    }));
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
   };
 
   const selectPhotoId = (id) => {
-    setState(prevState => ({
-      ...prevState,
-      currentPhoto: id
-    }));
+    dispatch({ type: ACTIONS.SELECT_PHOTO_ID, payload: { id } });
   };
-
+ 
+  // returns an object with our functions and state
   return {
     state,
     toggleFav,
